1c5727e17af8e09e19a404a7cdfefc3f555cf6ed
Masonn <mason@itsmason.online>
2025-04-25 10:19:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.login(process.env.TOKEN);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
+}).then(() => console.log('Connected to MongoDB!'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // License Schema and Model
